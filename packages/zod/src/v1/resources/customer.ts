dbd2b565d4bef761de64e481a0073c9a4e3b4898
almeida <hello@eualmeida.com.br>
2026-01-21 11:02:00 -0300

    feat(@abacatepay/zod): add v1 customer schemas

---
+++ packages/zod/src/v1/resources/customer.ts
@@ -0,0 +1,28 @@
+import { z } from 'zod';
+
+/**
+ * https://docs.abacatepay.com/pages/client/reference#estrutura
+ */
+export const APICustomer = z.object({
+	id: z.string({
+		description: 'Unique customer identifier.',
+	}),
+	metadata: z.object({
+		name: z.string({
+			description: "Customer's full name.",
+		}),
+		email: z
+			.string({
+				description: "Customer's email",
+			})
+			.email(),
+		taxId: z.string({
+			description: "Customer's CPF or CNPJ.",
+		}),
+		cellphone: z.string({
+			description: "Customer's cell phone.",
+		}),
+	}),
+});
+
+export type APICustomer = z.infer<typeof APICustomer>;
