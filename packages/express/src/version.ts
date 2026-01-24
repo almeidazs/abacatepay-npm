f2ab9015c731886b5dbc7f8a06eaa17724c581af
almeida <hello@eualmeida.com.br>
2026-01-24 12:30:48 -0300

    feat(@abacatepay/express): publish v1.0.0

---
+++ packages/express/src/version.ts
@@ -0,0 +1,6 @@
+// AUTO-GENERATED — DO NOT EDIT
+
+/**
+ * Current version of [@abacatepay/express](https://www.npmjs.com/package/@abacatepay/express).
+ */
+export const version = '1.0.0' as const;
