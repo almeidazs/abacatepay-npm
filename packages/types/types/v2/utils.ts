76d11ea6166dbd71df9e4cc35a01d1a82abeef43
almeida <hello@eualmeida.com.br>
2026-01-29 09:40:22 -0300

    feat(@abacatepay/types): export ABACATEPAY_SHARED_KEY and verifyWebhookSignature util function

--- packages/types/types/v2/utils.ts
+++ packages/types/types/v2/utils.ts
@@ -1,4 +1,3 @@
-import { createHmac, timingSafeEqual } from 'node:crypto';
 import { type WebhookEvent, WebhookEventType } from './webhook';
 
 /**
@@ -27,25 +26,3 @@ export function isPayoutFailedWebhookEvent(event: WebhookEvent) {
 export function isBillingPaidWebhookEvent(event: WebhookEvent) {
 	return event.event === WebhookEventType.BillingPaid;
 }
-
-const ABACATEPAY_PUBLIC_KEY =
-	't9dXRhHHo3yDEj5pVDYz0frf7q6bMKyMRmxxCPIPp3RCplBfXRxqlC6ZpiWmOqj4L63qEaeUOtrCI8P0VMUgo6iIga2ri9ogaHFs0WIIywSMg0q7RmBfybe1E5XJcfC4IW3alNqym0tXoAKkzvfEjZxV6bE0oG2zJrNNYmUCKZyV0KZ3JS8Votf9EAWWYdiDkMkpbMdPggfh1EqHlVkMiTady6jOR3hyzGEHrIz2Ret0xHKMbiqkr9HS1JhNHDX9';
-
-/**
- * Verify whether the signature is valid or not.
- * @param raw Raw body response (string).
- * @param signature The content of the`X-Webhook-Signature` header.
- * @returns A boolean that indicates if the signature is valid.
- */
-export const verifyWebhookSignature = (raw: string, signature: string) => {
-	const bodyBuffer = Buffer.from(raw, 'utf8');
-
-	const expectedSig = createHmac('sha256', ABACATEPAY_PUBLIC_KEY)
-		.update(bodyBuffer)
-		.digest('base64');
-
-	const A = Buffer.from(expectedSig);
-	const B = Buffer.from(signature);
-
-	return A.length === B.length && timingSafeEqual(A, B);
-};
